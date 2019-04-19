2dd8e7b14d596a969a1d568fc850e0cb7e816453
jchou8 <joseph98c@gmail.com>
2019-04-18 19:14:23 -0700

    basic layout, initial logic

--- app/src/main/java/edu/washington/jchou8/tipcalc/MainActivity.kt
+++ app/src/main/java/edu/washington/jchou8/tipcalc/MainActivity.kt
@@ -2,11 +2,24 @@ package edu.washington.jchou8.tipcalc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var amount = 12.34
+    private var percent = 0.15
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tipBtn = findViewById<Button>(R.id.btnTip)
+        tipBtn.setOnClickListener{
+            Toast.makeText(applicationContext, "$%.2f".format(calcTip()), Toast.LENGTH_LONG).show()
+        }
     }
+
+    private fun calcTip() = amount * (1 + percent)
 }
