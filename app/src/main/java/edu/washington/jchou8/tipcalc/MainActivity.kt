29d8d445b4de9f737649bcfa0e1cbd39b4248f9e
jchou8 <joseph98c@gmail.com>
2019-04-20 21:56:04 -0700

    implement spinner, add screenshot

--- app/src/main/java/edu/washington/jchou8/tipcalc/MainActivity.kt
+++ app/src/main/java/edu/washington/jchou8/tipcalc/MainActivity.kt
@@ -3,23 +3,65 @@ package edu.washington.jchou8.tipcalc
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.Toast
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.*
+
 
 class MainActivity : AppCompatActivity() {
-    private var amount = 12.34
-    private var percent = 0.15
+    private var amountStr = ""
+    private var amount = 0
+    private var percent = 15
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tipBtn = findViewById<Button>(R.id.btnTip)
-        tipBtn.setOnClickListener{
-            Toast.makeText(applicationContext, "$%.2f".format(calcTip()), Toast.LENGTH_LONG).show()
+        val btnTip = findViewById<Button>(R.id.btnTip)
+        val edtAmount = findViewById<EditText>(R.id.edtAmount)
+        val spnTipPct = findViewById<Spinner>(R.id.spnTipPct)
+
+        btnTip.isEnabled = false
+        btnTip.setOnClickListener{
+            Toast.makeText(applicationContext, calcTip(), Toast.LENGTH_LONG).show()
         }
+
+        edtAmount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(s.toString() != amountStr){
+                    edtAmount.removeTextChangedListener(this)
+                    val cleanString = s.toString().replace("[\$.]".toRegex(), "")
+                    amount = if (cleanString.toIntOrNull() == null) 0 else cleanString.toInt()
+                    val formatted = "$%.2f".format(amount * 0.01)
+                    amountStr = formatted
+                    edtAmount.setText(formatted)
+                    edtAmount.setSelection(formatted.length)
+                    edtAmount.addTextChangedListener(this)
+
+                    btnTip.isEnabled = amount > 0
+                }
+            }
+
+            override fun afterTextChanged(s: Editable) {
+            }
+        })
+
+        spnTipPct.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, pos: Int, id: Long) {
+                val selection = parent.getItemAtPosition(pos)
+                percent = selection.toString().replace("%", "").toInt()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+            }
+        }
+        spnTipPct.setSelection(1)
     }
 
-    private fun calcTip() = amount * (1 + percent)
+    private fun calcTip() = "$%.2f".format(amount * (percent * 0.01) * 0.01)
 }
